dc1953e4f933a0331b038d99550cf7ed1cb8ed9a
Gairren Boysel <RisenMatyr@gmail.com>
2017-05-05 12:14:32 -0500

    Speed
    I changed the speed to a value I was comfortable with.

--- src/game/Camera.java
+++ src/game/Camera.java
@@ -6,8 +6,8 @@ import java.awt.event.KeyListener;
 public class Camera implements KeyListener{
 	public double xPos, yPos, xDir, yDir, xPlane, yPlane;
 	public boolean left, right, forward, back;
-	public final double MOVE_SPEED = .08;
-	public final double ROTATION_SPEED = .045;
+	public final double MOVE_SPEED = .05;
+	public final double ROTATION_SPEED = .05;
 	public Camera(double x, double y, double xd, double yd, double xp, double yp) {
 		xPos = x;
 		yPos = y;
