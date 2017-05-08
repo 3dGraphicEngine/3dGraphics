6c97b57a03698e716268ad4d37422eaa3059178c
dbeehler <dbeehler00@gmail.com>
2017-05-08 12:55:08 -0500

    added two of the sound files going to be used

---
+++ mazegame/src/game/Camera.java
@@ -0,0 +1,76 @@
+package game;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class Camera implements KeyListener{
+	public double xPos, yPos, xDir, yDir, xPlane, yPlane;
+	public boolean left, right, forward, back;
+	public final double MOVE_SPEED = .05;
+	public final double ROTATION_SPEED = .05;
+	public Camera(double x, double y, double xd, double yd, double xp, double yp) {
+		xPos = x;
+		yPos = y;
+		xDir = xd;
+		yDir = yd;
+		xPlane = xp;
+		yPlane = yp;
+	}
+	public void keyPressed(KeyEvent key) {
+		if((key.getKeyCode() == KeyEvent.VK_A)){
+			left = true;
+			while (left = true){
+			}
+		}
+		if((key.getKeyCode() == KeyEvent.VK_D))
+			right = true;
+		if((key.getKeyCode() == KeyEvent.VK_W))
+			forward = true;
+		if((key.getKeyCode() == KeyEvent.VK_S))
+			back = true;
+	}
+	public void keyReleased(KeyEvent key) {
+		if((key.getKeyCode() == KeyEvent.VK_A))
+			left = false;
+		if((key.getKeyCode() == KeyEvent.VK_D))
+			right = false;
+		if((key.getKeyCode() == KeyEvent.VK_W))
+			forward = false;
+		if((key.getKeyCode() == KeyEvent.VK_S))
+			back = false;
+	}
+	public void update(int[][] map) {
+		if(forward) {
+			if(map[(int)(xPos + xDir * MOVE_SPEED)][(int)yPos] == 0) {
+				xPos+=xDir*MOVE_SPEED;
+			}
+			if(map[(int)xPos][(int)(yPos + yDir * MOVE_SPEED)] ==0)
+				yPos+=yDir*MOVE_SPEED;
+		}
+		if(back) {
+			if(map[(int)(xPos - xDir * MOVE_SPEED)][(int)yPos] == 0)
+				xPos-=xDir*MOVE_SPEED;
+			if(map[(int)xPos][(int)(yPos - yDir * MOVE_SPEED)]==0)
+				yPos-=yDir*MOVE_SPEED;
+		}
+		if(right) {
+			double oldxDir=xDir;
+			xDir=xDir*Math.cos(-ROTATION_SPEED) - yDir*Math.sin(-ROTATION_SPEED);
+			yDir=oldxDir*Math.sin(-ROTATION_SPEED) + yDir*Math.cos(-ROTATION_SPEED);
+			double oldxPlane = xPlane;
+			xPlane=xPlane*Math.cos(-ROTATION_SPEED) - yPlane*Math.sin(-ROTATION_SPEED);
+			yPlane=oldxPlane*Math.sin(-ROTATION_SPEED) + yPlane*Math.cos(-ROTATION_SPEED);
+		}
+		if(left) {
+			double oldxDir=xDir;
+			xDir=xDir*Math.cos(ROTATION_SPEED) - yDir*Math.sin(ROTATION_SPEED);
+			yDir=oldxDir*Math.sin(ROTATION_SPEED) + yDir*Math.cos(ROTATION_SPEED);
+			double oldxPlane = xPlane;
+			xPlane=xPlane*Math.cos(ROTATION_SPEED) - yPlane*Math.sin(ROTATION_SPEED);
+			yPlane=oldxPlane*Math.sin(ROTATION_SPEED) + yPlane*Math.cos(ROTATION_SPEED);
+		}
+	}
+	public void keyTyped(KeyEvent arg0) {
+		
+	}
+}
